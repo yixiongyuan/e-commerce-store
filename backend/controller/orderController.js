e78f7a485fbcb50dee95f42b3e536c829387037e
yixiongyuan <zachyyuan@gmail.com>
2023-09-18 14:39:10 -0400

    checkout function

---
+++ backend/controller/orderController.js
@@ -0,0 +1,104 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
+
+// @desc create new order
+// @route POST /api/orders
+// @access Private
+const addOrderItems = asyncHandler(async (req, res) => {
+
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+
+        const order = new Order({
+
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
+});
+
+// @desc get logged in user orders
+// @route GET /api/orders/myorders
+// @access Private
+const getMyOrders = asyncHandler(async (req, res) => {
+
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
+});
+
+// @desc get order by ID
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+
+    const order = await Order.find(req.params.id).populate('user', 'name email');
+    res.status(200).json(order);
+
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+
+// @desc update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+
+    res.send('update order to paid');
+});
+
+// @desc update order to delivered
+// @route GET /api/orders/:id/deliver
+// @access Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+
+    res.send('update order to delivered');
+});
+
+// @desc get all orders
+// @route GET /api/orders
+// @access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+
+    res.send('get all orders');
+});
+
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+};
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [] };
+const initialState = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 
 
@@ -30,9 +32,21 @@ const cartSlice = createSlice({
             state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
             return updateCart(state);
+        },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
+        },
+        savePaymentMethod: (state, action) => {
+            state.paymentMethod = action.payload;
+            return updateCart(state);
+        },
+        clearCartItems: (state, action) => {
+            state.cartItems = [];
+            return updateCart(state);
         }
     }
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod, clearCartItems } = cartSlice.actions;
 export default cartSlice.reducer;
