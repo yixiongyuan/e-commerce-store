e932f62ebfbda075eb4ce7805066ff03cf69c053
yixiongyuan <zachyyuan@gmail.com>
2023-09-20 16:24:49 -0400

    fine tune

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
 
 
 app.use('/api/products', productRoutes);
@@ -38,6 +35,19 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 
 const __dirname = path.resolve(); //set dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    //any route that is not api will be directed to index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
